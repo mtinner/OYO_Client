7c6d60ed04c6518d5378558a9c0da42e0026531e
Tobias Büchel <tobi.buechel@gmail.com>
2017-07-03 19:30:21 +0200

    Set dummy data

--- src/services/EndpointService.ts
+++ src/services/EndpointService.ts
@@ -1,12 +1,15 @@
 import { BaseService } from './BaseService';
 import { IIO } from '../common/IIO';
+
 export class EndpointService extends BaseService {
 
-	getEndpoints(params: object = {}): Promise<[IIO]> {
-		return this.get(`${this.baseApi}endpoints${this.objectToParam(params)}`);
+	DATA_ENDPOINT = { outputPin: 7, inputPin: 2, chipId: 10759249, id: '10759249_2', ip: '192.168.178.35', inputLevel: 0, status: 0, activated: true };
+
+	getEndpoints(params: object = {}): Promise<any> {
+		return Promise.resolve([this.DATA_ENDPOINT]);
 	}
 
-	updateEndpoint(io: IIO): Promise<IIO> {
-		return this.put(`${this.baseApi}endpoints/${io.id}`, io);
+	updateEndpoint(io: IIO): Promise<any> {
+		return Promise.resolve(this.DATA_ENDPOINT);
 	}
 }
